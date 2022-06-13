2ef200053040ddb6bbeea3ebf94f3ccd9ea1f3d1
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-13 15:59:53 +0700

    feat: Setup project config

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_MAP_URL,
+  generateRobotsTxt: true,
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
+const withPlugins = require('next-compose-plugins')
+const withAntdLess = require('next-plugin-antd-less')
+const withBundleAnalyzer = require('@next/bundle-analyzer')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  webpack(config) {
+    return config
+  },
 }
 
-module.exports = nextConfig
+const pluginAntdLess = withAntdLess({
+  modifyVars: {
+    '@primary-color': '#2F4858',
+    '@link-color': '#2F4858',
+    '@border-radius-base': '6px',
+    '@font-size-base': '16px',
+    '@text-color': '#3c3c3c',
+    '@heading-color': '#3c3c3c',
+    '@body-background': '#f1f1f1',
+  },
+})
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withPlugins([pluginAntdLess, bundleAnalyzer], {
+  nextConfig,
+})
