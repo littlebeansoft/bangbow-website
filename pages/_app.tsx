c8be3d35056ab51286748006582c571af7a44de1
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-14 10:33:02 +0700

    feat: Wrapped with config provider to setup the size of component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import 'antd/dist/antd.css'
+
+import '../styles/globals.css'
+
+import { ConfigProvider } from 'antd'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ConfigProvider componentSize="large">
+      <Component {...pageProps} />
+    </ConfigProvider>
+  )
 }
 
-export default MyApp
+export default App
