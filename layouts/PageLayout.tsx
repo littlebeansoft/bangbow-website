532847693dae09a3c31943047e35d6fd149f2667
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-14 15:17:20 +0700

    feat: Added seo content

--- layouts/PageLayout.tsx
+++ layouts/PageLayout.tsx
@@ -1,13 +1,35 @@
+import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 import styled from '@emotion/styled'
+
 import Container from 'components/Container'
 import Footer from 'components/Footer'
 import Navbar from 'components/Navbar'
 
 import type { FCWithChildren } from 'types'
 
-const PageLayout: FCWithChildren = ({ children }) => {
+interface PageLayoutProps {
+  title: string
+  description?: string
+}
+
+const PageLayout: FCWithChildren<PageLayoutProps> = ({
+  title,
+  description,
+  children,
+}) => {
   return (
     <PageLayoutContainer>
+      <NextSeo title={title} description={description} />
+
+      <Head>
+        <title>{title}</title>
+
+        <meta name="description" content={description} />
+
+        <link rel="shortcut icon" href="/icon-factory.svg" />
+      </Head>
+
       <Navbar />
 
       <Container
