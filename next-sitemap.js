f3f0900eca3e8cb0a21c8ac9369daf6420654437
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-13 20:53:02 +0700

    feat: Changed env to next public

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_MAP_URL,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_MAP_URL,
   generateRobotsTxt: true,
 }
