d4712f91d0954f3f15e522b18e1e204bc7868b50
kittipongkp <kittipong.na@littlebeansoft.com>
2022-09-17 15:46:46 +0700

    otp

--- components/PhoneNumberInput.tsx
+++ components/PhoneNumberInput.tsx
@@ -2,12 +2,15 @@ import type { ChangeEvent, FC } from 'react'
 
 import isNumeric from 'validator/lib/isNumeric'
 import isMobilePhone from 'validator/lib/isMobilePhone'
-import { Input } from 'antd'
+import { Input, message } from 'antd'
 import { useRouter } from 'next/router'
 
 import Text from 'components/Text'
 
 import { getPageTypeTheme } from 'helpers/utils'
+import { useRequestOtpMutation } from 'graphql/_generated/operations'
+import { useAppSelector } from 'store'
+import { CheckCircleOutlined } from '@ant-design/icons'
 
 interface PhoneNumberInputProps {
   value?: string
@@ -23,6 +26,22 @@ const PhoneNumberInput: FC<PhoneNumberInputProps> = ({
   onVisibleMobileOTP,
 }) => {
   const router = useRouter()
+
+  const otpVerify = useAppSelector((state) => state.otp.otpVerify)
+
+  const [requestOtp] = useRequestOtpMutation({
+    context: {
+      clientType: 'CORE',
+      headers: {
+        credentialKey: 'BANG_BOW_ADMIN',
+      },
+    },
+    onCompleted: (data) => {
+      message.success('OTP sent successfully')
+      onVisibleMobileOTP()
+      console.log('data', data.requestOtp.payload)
+    },
+  })
 
   const hideRequestOTPButton =
     value == null || !isMobilePhone(value) || value.length !== 10
@@ -43,16 +62,37 @@ const PhoneNumberInput: FC<PhoneNumberInputProps> = ({
       maxLength={10}
       placeholder="เบอร์โทรผู้ติดต่อ"
       value={value}
+      disabled={otpVerify}
       suffix={
-        <Text
-          hidden={hideRequestOTPButton}
-          page={page}
-          color="primary"
-          weight={500}
-          onClick={onVisibleMobileOTP}
-        >
-          ส่งรหัส
-        </Text>
+        otpVerify ? (
+          <>
+            <CheckCircleOutlined style={{ color: 'green' }} />
+            <Text
+              hidden={hideRequestOTPButton}
+              page={page}
+              color="primary"
+              weight={500}
+            >
+              ยืนยันแล้ว
+            </Text>
+          </>
+        ) : (
+          <Text
+            hidden={hideRequestOTPButton}
+            page={page}
+            color="primary"
+            weight={500}
+            onClick={() => {
+              requestOtp({
+                variables: {
+                  phoneNumber: value || '',
+                },
+              })
+            }}
+          >
+            ส่งรหัส
+          </Text>
+        )
       }
       onChange={handleChangeNumberOnly}
     />
