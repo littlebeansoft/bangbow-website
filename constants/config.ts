04e6fab7add45055e80f69ae0d3cfdb0a5b46198
nuttakulBeanX <138424582+nuttakulBeanX@users.noreply.github.com>
2024-09-02 17:43:41 +0700

    Update config.ts

--- constants/config.ts
+++ constants/config.ts
@@ -1,5 +1,5 @@
 export const backendUrl =
   process.env.NEXT_PUBLIC_BACKEND_URL ||
-  'https://api-bangbow.azurewebsites.net'
+  'https://bangbow-api-dev-v2.azurewebsites.net'
 
 export const allowFileExtensionsImage = '.jpg, .jpeg, .png'
